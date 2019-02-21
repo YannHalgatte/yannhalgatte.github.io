f85cdd5dd712285ce2599a361036d745a84169ec
YannHalgatte <halgatte.yann@gmail.com>
2019-02-21 14:09:58 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -54,3 +54,26 @@ var TxtRotate = function(el, toRotate, period) {
     css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
   };
+
+
+
+  // JQUERY
+
+  $(function(){
+    $(window).scroll( function(){
+      
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      //fade-in
+      $('.fade-ani').each(function(){
+        var bottom_of_object = $(this).position().top+250
+        //  + $(this).outerHeight();
+         if( bottom_of_window > bottom_of_object ){
+          $(this).addClass('showing');
+        }
+        else{
+          $(this).removeClass('showing');
+        }
+      });
+  
+    });
+  });
